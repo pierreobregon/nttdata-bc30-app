bc42acbc0585f0630fb412570542b6603aab99b0
Pierre Obregon <pierre.obregon@gmail.comierre Obregon>
2022-08-08 12:36:52 -0500

    inicial

---
+++ src/main/java/com/nttdata/bc30/appbc30/AppBc30Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.bc30.appbc30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBc30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBc30Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bc30/appbc30/AppBc30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bc30.appbc30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBc30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
